72b97610824d64661fd4c8a8fe1f97cdcfdc9035
fleroux <francois.leroux.pro@gmail.com>
2024-12-03 17:06:56 +0100

    modulated pywfs working. next step: setup the energy conservation

--- explorations/modulated_pywfs.py
+++ explorations/modulated_pywfs.py
@@ -35,26 +35,39 @@ def get_tilt(shape, theta=0., amplitude=1.):
 def get_4pywfs_phase_mask(shape, amplitude):
     
     mask = np.empty(shape, dtype=float)
-    [X,Y] = np.meshgrid(np.arange(0, shape[0]), np.arange(0, shape[1]))
-    Y = np.flip(Y, axis=0) # change orientation
+    theta_list = [[np.pi/4, 3*np.pi/4], [7*np.pi/4, 5* np.pi/4]]
     
-    theta = [np.pi/4, 3*np.pi/4, 5*np.pi/4, 7*np.pi/4]
-    mask[:mask.shape[0]//2, mask.shape[1]//2:] = (np.cos(theta[0]) * X + np.sin(theta[0]) * Y)[:mask.shape[0]//2, mask.shape[1]//2:]
-    mask[:mask.shape[0]//2, :mask.shape[1]//2] = (np.cos(theta[1]) * X + np.sin(theta[1]) * Y)[:mask.shape[0]//2, :mask.shape[1]//2]
-    mask[mask.shape[0]//2:, :mask.shape[1]//2] = (np.cos(theta[2]) * X + np.sin(theta[2]) * Y)[mask.shape[0]//2:, mask.shape[1]//2:]
-    mask[mask.shape[0]//2:, mask.shape[1]//2:] = (np.cos(theta[3]) * X + np.sin(theta[3]) * Y)[:mask.shape[0]//2, mask.shape[1]//2:]
-    
-    mask[:mask.shape[0]//2, mask.shape[1]//2:] = (mask[:mask.shape[0]//2, mask.shape[1]//2:]-mask[:mask.shape[0]//2, mask.shape[1]//2:].min())/(mask[:mask.shape[0]//2, mask.shape[1]//2:].max()-mask[:mask.shape[0]//2, mask.shape[1]//2:].min())
-    mask[:mask.shape[0]//2, :mask.shape[1]//2] = (mask[:mask.shape[0]//2, :mask.shape[1]//2]-mask[:mask.shape[0]//2, :mask.shape[1]//2].min())/(mask[:mask.shape[0]//2, :mask.shape[1]//2].max()-mask[:mask.shape[0]//2, :mask.shape[1]//2].min())
-    mask[mask.shape[0]//2:, :mask.shape[1]//2] = (mask[mask.shape[0]//2:, :mask.shape[1]//2]-mask[mask.shape[0]//2:, :mask.shape[1]//2].min())/(mask[mask.shape[0]//2:, :mask.shape[1]//2].max()-mask[mask.shape[0]//2:, :mask.shape[1]//2].min())
-    mask[mask.shape[0]//2:, mask.shape[1]//2:] = (mask[mask.shape[0]//2:, mask.shape[1]//2:]-mask[mask.shape[0]//2:, mask.shape[1]//2:].min())/(mask[mask.shape[0]//2:, mask.shape[1]//2:].max()-mask[mask.shape[0]//2:, mask.shape[1]//2:].min())
+    for m in range(len(theta_list)):
+        for n in range(len(theta_list[0])):
+            tilt = get_tilt([0.5*shape[0], 0.5*shape[1]], theta=theta_list[m][n], amplitude = amplitude)
+            mask[m*mask.shape[0]//2:(m+1)*mask.shape[0]//2, n*mask.shape[0]//2:(n+1)*mask.shape[0]//2] = tilt
+            
+    return mask
 
-    return amplitude*mask
+def get_modulation_phase_screens(pupil, n_modulation_points, modulation_radius):
+    
+    modulation_phase_screens = np.empty((pupil.shape[0], pupil.shape[1], n_modulation_points), dtype=float)
+    
+    tilt_amplitude = 2.*np.pi * modulation_radius
+    
+    theta_list = np.arange(0., 2.*np.pi, 2.*np.pi/n_modulation_points)
+    
+    for k in range(n_modulation_points):
+        
+        theta = theta_list[k]  
+        tilt_theta = get_tilt((pupil.shape[0], pupil.shape[1]), theta)
+        tilt_theta[pupil>0] = (tilt_theta[pupil>0] - tilt_theta[pupil>0].min())\
+            /(tilt_theta[pupil>0].max()- tilt_theta[pupil>0].min())
+        tilt_theta[pupil==0] = 0.
+        modulation_phase_screens[:,:,k] = tilt_amplitude*tilt_theta
+        
+    return modulation_phase_screens
+
 
 def get_focal_plane_image(complex_amplitude):
     return np.abs(np.fft.fftshift(np.fft.fft2(complex_amplitude)))**2
 
-def propagate_fourier_filtering(complex_amplitude, mask_complex_amplitude):
+def get_ffwfs_frame(complex_amplitude, mask_complex_amplitude):
     focal_plane_complex_amplitude = np.fft.fftshift(np.fft.fft2(complex_amplitude))*mask_complex_amplitude
     return np.abs(np.fft.fft2(np.fft.ifftshift(focal_plane_complex_amplitude)))**2
     
@@ -77,29 +90,7 @@ pupil = get_circular_pupil(npx)
 
 #%% ---------------- MODULATOR --------------------
 
-theta_list = np.arange(0., 2.*np.pi, 2.*np.pi/n_modulation_points)
-tilt_amplitude = 2.*np.pi * modulation_radius
-
-# modulation phase screens generation - Units : phase
-modulation_phase_screens = np.empty((pupil.shape[0],
-                                     pupil.shape[1],
-                                     n_modulation_points), dtype=float)
-[X,Y] = np.meshgrid(np.arange(0, pupil.shape[0]), np.arange(0, pupil.shape[0]))
-Y = np.flip(Y, axis=0) # change orientation
-
-for k in range(n_modulation_points):
-
-    theta = theta_list[k]    
-
-    tilt_theta = np.cos(theta) * X + np.sin(theta) * Y
-
-    # pupil addition and normalization
-    
-    tilt_theta[pupil>0] = (tilt_theta[pupil>0] - tilt_theta[pupil>0].min())\
-        /(tilt_theta[pupil>0].max()- tilt_theta[pupil>0].min())
-    tilt_theta[pupil==0] = 0.
-    
-    modulation_phase_screens[:,:,k] = tilt_amplitude*tilt_theta
+modulation_phase_screens = get_modulation_phase_screens(pupil, n_modulation_points, modulation_radius)
     
 # %% ------------------ FOCAL PLANE MASK -----------
 
@@ -116,10 +107,29 @@ phasor = 1/zeros_padding_factor * 2**0.5 * np.pi * phasor
 
 # %% ------------------ PROPAGATION ------------------
 
+#%% Non-modulated pywfs
 pupil_pad = zeros_padding(pupil*np.exp(1j*phasor), zeros_padding_factor)
 
 focal_plane_detector = get_focal_plane_image(pupil_pad)
 
 mask_complex_amplitude = np.exp(1j*mask)
 
-pywfs_detector = propagate_fourier_filtering(pupil_pad, mask_complex_amplitude)
+pywfs_detector = get_ffwfs_frame(pupil_pad, mask_complex_amplitude)
+
+#%% Modulated pywfs
+
+mpywfs_detector = np.zeros([zeros_padding_factor*pupil.shape[0], zeros_padding_factor*pupil.shape[1]])
+gsc_detector = np.zeros([zeros_padding_factor*pupil.shape[0], zeros_padding_factor*pupil.shape[1]])
+
+for k in range(modulation_phase_screens.shape[2]):
+    
+    pupil_pad = zeros_padding(pupil*np.exp(1j*phasor)*np.exp(1j*modulation_phase_screens[:,:,k]), zeros_padding_factor)
+    mpywfs_detector += get_ffwfs_frame(pupil_pad, mask_complex_amplitude)
+    
+    gsc_detector += get_focal_plane_image(pupil_pad)
+
+plt.figure(1)
+plt.imshow(mpywfs_detector)
+
+plt.figure(2)
+plt.imshow(gsc_detector)
