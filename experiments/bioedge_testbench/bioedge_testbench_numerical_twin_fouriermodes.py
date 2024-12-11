e96823e24b6376998d34a6b76c1bd2065bc99363
fleroux <francois.leroux.pro@gmail.com>
2024-12-11 18:41:41 +0100

    sauvegarde

---
+++ experiments/bioedge_testbench/bioedge_testbench_numerical_twin_fouriermodes.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 10:08:36 2024
+
+@author: fleroux
+"""
+
+from copy import deepcopy
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+n_subaperture = 16 # 124
+
+
+
+#%% Functions declarations
+
+def get_tilt(shape, theta=0., amplitude=1.):
+    [X,Y] = np.meshgrid(np.arange(0, shape[0]), np.arange(0, shape[1]))
+    tilt_theta = np.cos(theta) * X + np.sin(theta) * Y
+    Y = np.flip(Y, axis=0) # change orientation
+    tilt_theta = (tilt_theta - tilt_theta.min())/(tilt_theta.max()- tilt_theta.min())
+    
+    return amplitude*tilt_theta
+
+#%% -----------------------     TELESCOPE   ----------------------------------
+from OOPAO.Telescope import Telescope
+
+# create the Telescope object
+tel = Telescope(resolution           = 4*n_subaperture,   # resolution of the telescope in [pix]
+                diameter             = 8,                 # diameter in [m]        
+                samplingTime         = 1/1000,            # Sampling time in [s] of the AO loop
+                centralObstruction   = 0.,                # Central obstruction in [%] of a diameter 
+                display_optical_path = False,             # Flag to display optical path
+                fov                  = 10)               # field of view in [arcsec]. If set to 0 (default) this speeds up the computation of 
+                                                          # the phase screens but is uncompatible with off-axis targets
+
+#%% -----------------------     NGS   ----------------------------------
+
+# GHOST wavelength : 770 nm, full bandwidth = 20 nm
+# 'I2' band : 750 nm, bandwidth? = 33 nm
+
+from OOPAO.Source import Source
+
+# create the Natural Guide Star object
+ngs = Source(optBand     = 'I2',          # Optical band (see photometry.py)
+             magnitude   = 0,             # Source Magnitude
+             coordinates = [0,0])         # Source coordinated [arcsec,deg]
+
+# combine the NGS to the telescope using '*'
+ngs*tel
+
+#%% Compute PSF
+
+tel.computePSF(zeroPaddingFactor=2)
+
+#%% -----------------------     Bi-O-Edge WFS   ------------------------
+
+from OOPAO.BioEdge import BioEdge
+
+wfs = BioEdge(nSubap = n_subaperture, 
+              telescope = tel, 
+              modulation = 0.,
+              grey_width = 2.5, 
+              lightRatio = 0.5,
+              n_pix_separation = 0,
+              postProcessing = 'fullFrame', 
+              psfCentering=False)
+
+# propagate the light to the Wave-Front Sensor
+tel*wfs
+wfs.wfs_measure(tel.pupil)
+flat_frame = wfs.cam.frame
+
+
+#%% Deformable mirror
+
+from OOPAO.DeformableMirror import DeformableMirror
+
+dm = DeformableMirror(tel, nSubap=2*n_subaperture)
+
+#%% Atmosphere
+
+from OOPAO.Atmosphere import Atmosphere
+           
+# create the Atmosphere object
+atm = Atmosphere(telescope     = tel,                               # Telescope                              
+                 r0            = 0.15,                              # Fried Parameter [m]
+                 L0            = 25,                                # Outer Scale [m]
+                 fractionalR0  = [0.45 ,0.1  ,0.1  ,0.25  ,0.1   ], # Cn2 Profile
+                 windSpeed     = [10   ,12   ,11   ,15    ,20    ], # Wind Speed in [m]
+                 windDirection = [0    ,72   ,144  ,216   ,288   ], # Wind Direction in [degrees]
+                 altitude      = [0    ,1000 ,5000 ,10000 ,12000 ]) # Altitude Layers in [m]
+
+
+#%% Modal basis
+
+from OOPAO.tools.tools import compute_fourier_mode
+
+# poke
+M2C = np.zeros((dm.nValidAct, dm.nValidAct))
+
+for m in range(dm.nValidAct):
+    M2C[m,m] = 1
+
+#%% Callibration - No SR
+
+from OOPAO.calibration.InteractionMatrix import InteractionMatrix
+
+stroke = 100e-9 # [m]
+
+tel.resetOPD()
+ngs*tel*dm
+calib = InteractionMatrix(ngs, atm, tel, dm, wfs, M2C = M2C, stroke = stroke)
+
+#%% Super Resolution
+
+sr_amplitude = 0.5 # pixel
+
+# sx = [-sr_amplitude, sr_amplitude, -sr_amplitude, sr_amplitude]
+# sy = [sr_amplitude, sr_amplitude, -sr_amplitude, -sr_amplitude]
+
+# sx = [-sr_amplitude, sr_amplitude, 0., 0.]
+# sy = [0. , 0. , -sr_amplitude, sr_amplitude]
+
+sx = [0. , 0. , -sr_amplitude, sr_amplitude]
+sy = [-sr_amplitude, sr_amplitude, 0., 0.]
+
+sx = np.array(sx)
+sy = np.array(sy)
+
+mask = wfs.mask
+mask_sr_cam_frame = deepcopy(mask)
+
+binning_factor = wfs.bioFrame.shape[0]/wfs.cam.frame.shape[0]
+sx_cam_frame = [binning_factor * k for k in sx]
+sy_cam_frame = [binning_factor * k for k in sy]
+
+for k in range(len(mask)):
+    tilt_x = get_tilt(mask[k].shape, theta = 0., amplitude = 2.*np.pi*sx_cam_frame[k])
+    tilt_y = get_tilt(mask[k].shape, theta = np.pi/2, amplitude = 2.*np.pi*sy_cam_frame[k])
+    mask_sr_cam_frame[k] = mask[k]*np.exp(1j*(tilt_x+tilt_y))
+
+# replace usual-masks by SR-masks
+wfs.mask = mask_sr_cam_frame
+wfs.modulation = 0. # update reference intensities etc.
+
+tel*wfs
+wfs.wfs_measure(tel.pupil)
+flat_frame_sr = wfs.cam.frame
+
+#%% Callibration - SR
+
+tel.resetOPD()
+calib_sr = InteractionMatrix(ngs, atm, tel, dm, wfs, M2C = M2C, stroke = stroke)
+
+# %% ------------------ PLOTS --------------------------------------------
+
+from OOPAO.tools.displayTools import display_wfs_signals
+
+plt.figure(1)
+plt.imshow(np.abs(flat_frame_sr-flat_frame))
+plt.title('SR pupils - No SR pupils (reference signal for a flat wavefront)\n0.25 pixel shifts')
+
+plt.figure(2)
+plt.plot(calib.eigenValues/calib.eigenValues.max(), 'b', label='no SR')
+plt.plot(calib_sr.eigenValues/calib_sr.eigenValues.max(), 'r', label='SR')
+plt.title('calib.eigenValues, '+str(n_subaperture)+' wfs subapertures, poke basis used, '+str(sr_amplitude) +' pixels shift')
+plt.legend()
+plt.xlabel('# eigen mode')
+plt.ylabel('normalized eigen value')
+
+#%%
+
+n_mode = 400
+
+plt.figure(3)
+display_wfs_signals(wfs, signals=calib_sr.D[:,n_mode])
+plt.title('Bi-O-Edge signal, '+str(n_mode)+'th mode, poke basis')
+
