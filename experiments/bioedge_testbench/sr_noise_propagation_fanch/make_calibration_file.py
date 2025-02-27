23c694a09346ce471c40c6e09237db844a53ae86
fleroux <francois.leroux.pro@gmail.com>
2025-02-27 16:05:07 +0100

    minor changes

--- experiments/bioedge_testbench/sr_noise_propagation_fanch/make_calibration_file.py
+++ experiments/bioedge_testbench/sr_noise_propagation_fanch/make_calibration_file.py
@@ -25,34 +25,44 @@ dill.load_session(param['path_object'] / Path('object'+str(param['filename'])+'.
 
 #%% Make calibrations
 
-calib_pyramid = InteractionMatrix(ngs, atm, tel, dm, pyramid, M2C = M2C, stroke = param['stroke'], single_pass=False)
-calib_pyramid_sr = InteractionMatrix(ngs, atm, tel, dm, pyramid_sr, M2C = M2C, stroke = param['stroke'], single_pass=False)
-calib_pyramid_oversampled = InteractionMatrix(ngs, atm, tel, dm, pyramid_oversampled, M2C = M2C, 
-                                              stroke = param['stroke'], single_pass=False)
+# calib_pyramid = InteractionMatrix(ngs, atm, tel, dm, pyramid, M2C = M2C, stroke = param['stroke'], single_pass=False, display=True)
+# calib_pyramid_sr = InteractionMatrix(ngs, atm, tel, dm, pyramid_sr, M2C = M2C, stroke = param['stroke'], single_pass=False, display=True)
+# calib_pyramid_oversampled = InteractionMatrix(ngs, atm, tel, dm, pyramid_oversampled, M2C = M2C, 
+#                                               stroke = param['stroke'], single_pass=False, display=True)
 
 #%%
 
 calib_gbioedge = InteractionMatrix(ngs, atm, tel, dm, gbioedge, M2C = M2C, 
-                                   stroke = param['stroke'], single_pass=param['single_pass'])
-calib_gbioedge_sr = InteractionMatrix(ngs, atm, tel, dm, gbioedge_sr, M2C = M2C, 
-                                   stroke = param['stroke'], single_pass=param['single_pass'])
-calib_gbioedge_oversampled = InteractionMatrix(ngs, atm, tel, dm, gbioedge_oversampled, M2C = M2C, 
-                                   stroke = param['stroke'], single_pass=param['single_pass'])
+                                   stroke = param['stroke'], single_pass=param['single_pass'], display=True)
+# calib_gbioedge_sr = InteractionMatrix(ngs, atm, tel, dm, gbioedge_sr, M2C = M2C, 
+#                                    stroke = param['stroke'], single_pass=param['single_pass'], display=True)
+# calib_gbioedge_oversampled = InteractionMatrix(ngs, atm, tel, dm, gbioedge_oversampled, M2C = M2C, 
+#                                    stroke = param['stroke'], single_pass=param['single_pass'], display=True)
 
-calib_sbioedge = InteractionMatrix(ngs, atm, tel, dm, sbioedge, M2C = M2C, 
-                                   stroke = param['stroke'], single_pass=param['single_pass'])
-calib_sbioedge_sr = InteractionMatrix(ngs, atm, tel, dm, sbioedge_sr, M2C = M2C, 
-                                   stroke = param['stroke'], single_pass=param['single_pass'])
-calib_sbioedge_oversampled = InteractionMatrix(ngs, atm, tel, dm, sbioedge_oversampled, M2C = M2C, 
-                                   stroke = param['stroke'], single_pass=param['single_pass'])
+# calib_sbioedge = InteractionMatrix(ngs, atm, tel, dm, sbioedge, M2C = M2C, 
+#                                    stroke = param['stroke'], single_pass=param['single_pass'], display=True)
+# calib_sbioedge_sr = InteractionMatrix(ngs, atm, tel, dm, sbioedge_sr, M2C = M2C, 
+#                                    stroke = param['stroke'], single_pass=param['single_pass'], display=True)
+# calib_sbioedge_oversampled = InteractionMatrix(ngs, atm, tel, dm, sbioedge_oversampled, M2C = M2C, 
+#                                    stroke = param['stroke'], single_pass=param['single_pass'], display=True)
 
 #%% remove all the variables we do not want to save in the pickle file provided by dill.load_session
 
 for obj in dir():
     #checking for built-in variables/functions
-    if not (obj.startswith('calib') or obj.startswith('__')):
+    if not (obj.startswith('calib') or obj.startswith('__') or obj.startswith('param')):
         #deleting the said obj, since a user-defined function
         del globals()[obj]
 del obj      
 
 print(dir())
+
+#%% save all variables
+
+from pathlib import Path
+
+import dill
+
+origin = Path(__file__) # keep a trace of where the saved objects come from
+
+dill.dump_session(param['path_calibration'] / Path('calibration'+str(param['filename'])+'.pkl'))
