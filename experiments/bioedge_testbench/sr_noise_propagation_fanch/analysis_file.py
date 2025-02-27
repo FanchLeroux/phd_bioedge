a248d7f5fc8dfeb349be42717bc9ef007c6c9433
fleroux <francois.leroux.pro@gmail.com>
2025-02-27 18:13:27 +0100

    to cluster

--- experiments/bioedge_testbench/sr_noise_propagation_fanch/analysis_file.py
+++ experiments/bioedge_testbench/sr_noise_propagation_fanch/analysis_file.py
@@ -5,3 +5,51 @@ Created on Wed Feb 26 13:46:18 2025
 @author: fleroux
 """
 
+# pylint: disable=undefined-variable
+
+import platform
+import pathlib
+
+from copy import deepcopy
+
+import dill
+
+from parameter_file import get_parameters
+
+
+#%% Import parameter file
+
+param = get_parameters()
+
+#%% Load OOPAO calibration objects computed in make_calibration_file.py
+
+if platform.system() == 'Windows':
+    temp = deepcopy(pathlib.PosixPath)
+    pathlib.PosixPath = pathlib.WindowsPath
+
+dill.load_session(param['path_calibration'] / pathlib.Path('calibration'+str(param['filename'])+'.pkl'))
+
+if platform.system() == 'Windows':
+    pathlib.PosixPath = temp
+
+#%% Analysis
+
+# extract singular values
+
+singular_values_gbioedge = calib_gbioedge.eigenValues
+
+# Calibration matrix truncation, inversion and noise propagation computation
+
+R_gbiedge_oversampled = np.linalg.pinv(calib_bioedge_oversampled.D)
+noise_propagation_gbioedge_oversampled = np.diag(R_gbiedge_oversampled @ R_gbiedge_oversampled.T)/wfs_oversampled.nSignal
+
+for n_modes in range(param['list_modes_to_keep'].shape[0]):
+
+    n_modes_no_sr = n_modes_list[n_modes]
+    n_modes_sr = n_modes_list[n_modes]
+
+    R = np.linalg.pinv(calib.D[:,:n_modes_no_sr])
+    R_sr = np.linalg.pinv(calib_sr.D[:,:n_modes_sr])
+
+    noise_propagation_no_sr.append(np.diag(R @ R.T)/wfs.nSignal)
+    noise_propagation_sr.append(np.diag(R_sr @ R_sr.T)/wfs.nSignal)
