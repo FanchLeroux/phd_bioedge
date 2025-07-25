ad27901230d391bd7e8d099853b5507e22321def
fleroux <francois.leroux.pro@gmail.com>
2025-07-25 11:48:08 +0200

    .sh script to run local scripts in tmux sessions on loom

--- dev/loom_run_scripts/run_hello_world_loom.sh
+++ dev/loom_run_scripts/run_hello_world_loom.sh
@@ -1,5 +1,25 @@
-# ssh loom python /net/SRVSTK12C/harmoni/fleroux/test.py
-# ssh loom bash -c /net/SRVSTK12C/harmoni/fleroux/run_test2.sh
+#!/bin/bash
 
-ssh "loom" "bash -c 'source /net/SRVSTK12C/harmoni/fleroux/code/environments/env-francois/bin/activate && 
-bash /net/SRVSTK12C/harmoni/fleroux/run_test2.sh'"
+# === CONFIGURATION ===
+REMOTE_USER="fleroux"
+REMOTE_HOST="loom-elt2.lam.fr"
+REMOTE_VENV="/net/SRVSTK12C/harmoni/fleroux/code/environments/env-francois"
+
+# Local script to upload
+LOCAL_SCRIPT="./hello_world.py"
+
+# Remote destination for the script
+REMOTE_TMP_SCRIPT="/net/SRVSTK12C/harmoni/fleroux/code/tmp/test_remote.py"
+
+# === COPY SCRIPT TO REMOTE MACHINE ===
+echo "Uploading ${LOCAL_SCRIPT} to ${REMOTE_HOST}:${REMOTE_TMP_SCRIPT}..."
+scp "${LOCAL_SCRIPT}" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_TMP_SCRIPT}"
+
+# === RUN SCRIPT ON REMOTE INSIDE VENV ===
+echo "Running script on remote host..."
+ssh "${REMOTE_USER}@${REMOTE_HOST}" "bash -c 'source \"${REMOTE_VENV}/bin/activate\" && python \"${REMOTE_TMP_SCRIPT}\"'"
+
+# === CLEANUP  ===
+echo "Cleaning up remote script..."
+ssh "${REMOTE_USER}@${REMOTE_HOST}" "rm -f \"${REMOTE_TMP_SCRIPT}\""
+echo "done."
